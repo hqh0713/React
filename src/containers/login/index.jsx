7b1ce5bf14ac11bb44ba1bf3a959799e70328562
huangqinghai <877489469@qq.com>
2019-07-19 18:34:10 +0800

    redux

--- src/containers/login/index.jsx
+++ src/containers/login/index.jsx
@@ -1,22 +1,59 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import './index.css'
-import { Button, NavBar, Icon } from 'antd-mobile'
+import { Button, NavBar, Icon, Toast } from 'antd-mobile'
+import store from '../../store/index'
+import './index.less'
 
 export default class Login extends React.Component {
-  componentWillReceiveProps(){
-    console.log('ddd')
+  constructor(props) {
+    super(props);
+    this.state = {
+      userAccount: ''
+    }
+    this.storeChange = this.storeChange.bind(this)
+    // 订阅store的变化
+    store.subscribe(this.storeChange)
   }
+
   render() {
     return(
       <div className='login'>
         <NavBar
           mode='light'
           icon={<Icon type='left'/>}>
-          登陆</NavBar>
-        <Button type='primary'>primary</Button>
-        <Link to='/'>首页</Link>
+          登陆
+        </NavBar>
+        <input 
+          placeholder='请输入账号'
+          maxLength='20' 
+          className='user-account'
+          onChange={this.changeUser.bind(this)}>
+          </input>
+        <Button type='primary' onClick={() => {this.login()}}>登陆</Button>
       </div>
     )
+  }
+  changeUser(e) {
+    this.setState({
+      userAccount: e.target.value
+    })
+  }
+  login() {
+    if(!this.state.userAccount.length) {
+      Toast.info('请输入账号')
+      return
+    }
+    const action = {
+      type: 'changeUserAccount',
+      value: this.state.userAccount
+    }
+    store.dispatch(action)
+    this.props.history.push({
+      pathname: '/'
+    })
+  }
+  storeChange() {
+    this.setState({
+      userAccount: store.getState().userName
+    })
   }
 }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,12 @@
 const defaultState = {
-  initNum: 0
-}  //默认数据
+  userName: 'we' //登陆账号
+}
 export default (state = defaultState,action)=>{  //就是一个方法函数
+  console.log(state, action)
+    if(action.type === 'changeUserAccount') {
+      let newState = JSON.parse(JSON.stringify(state))
+      newState.userName = action.value
+      return newState
+    }
     return state
 }
